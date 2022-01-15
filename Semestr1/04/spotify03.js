8addd943ddb8698fc7b266766123e13d4f2b1f3c
Paweł Łukaszuk <pawel-github@lukaszuk.net>
2022-01-15 17:30:29 +0100

    Code examples from fourth lecture

---
+++ Semestr1/04/spotify03.js
@@ -0,0 +1,83 @@
+// FUNCTIONS
+
+// function get(url, header, param, success) {...}
+// function post(url, header, param, success) {...}
+var tokenType, accessToken, userId, playlistId, songs = JSON.parse(localStorage.songs);
+
+function retrieveAccessToken(callback) {
+    post("https://accounts.spotify.com/api/token", {}, urlencode({
+        grant_type: 'authorization_code',
+        code: getParam(tab.url, 'code'),
+        redirect_uri: "https://www.lukaszuk.net/sfy.html",
+        client_id: ":)",
+        client_secret: ":)"
+    }), function (response) {
+        callback(response);
+    });
+}
+
+function retrieveUserId(response, callback) {
+    tokenType = response.token_type;
+    accessToken = response.access_token;
+    get("https://api.spotify.com/v1/me", {
+        Authorization: tokenType + ' ' + accessToken
+    }, null, function (response) {
+        callback(response);
+    });
+}
+
+function createANewPlaylist(response, callback) {
+    userId = response.id;
+    post("https://api.spotify.com/v1/users/" + userId + "/playlists", {
+        Authorization: tokenType + ' ' + accessToken,
+        "Content-type": "application/json"
+    }, JSON.stringify({
+        name: localStorage.playlistTitle
+    }), function (response) {
+        callback(response);
+    });
+}
+
+function searchASong(key, callback) {
+    get("https://api.spotify.com/v1/search", {
+        Authorization: tokenType + ' ' + accessToken
+    }, "q=" + songs[key].title + "%20album:" + songs[key].album + "%20artist:" + songs[key].artist + "&type=track", function (response) {
+        callback(response);
+    });
+}
+
+function addASongToThePlaylist(uri, callback) {
+    post("https://api.spotify.com/v1/users/" + userId + "/playlists/" + playlistId + "/tracks", {
+        Authorization: tokenType + ' ' + accessToken,
+        "Content-type": "application/json"
+    }, JSON.stringify({
+        uris: [uri]
+    }), function (response) {
+        callback(response);
+    });
+}
+
+function addAllSongsToPlayList(response, callback) {
+    playlistId = response.id;
+    var i = 0;
+    for (key in songs) {
+        searchASong(key, function (response) {
+            if (response.tracks.items.length) {
+                addASongToThePlaylist(response.tracks.items[0].uri, function (response) {
+                    i++;
+                });
+            }
+        });
+    }
+    callback(i);
+}
+
+retrieveAccessToken(function (response) {
+    retrieveUserId(response, function (response) {
+        createANewPlaylist(response, function (response) {
+            addAllSongsToPlayList(response, function (total) {
+                console.log("There are " + total + " out of " + songs.length + " songs been added to the playlist!!!");
+            });
+        });
+    });
+});
